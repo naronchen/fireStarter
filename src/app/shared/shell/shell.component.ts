ca9a249a237f8624c75d55952b180388d807c0ba
Naron <naron.chen@richmond.edu>
2023-09-19 15:23:34 -0400

    navigation done

---
+++ src/app/shared/shell/shell.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-shell',
+  templateUrl: './shell.component.html',
+  styleUrls: ['./shell.component.scss']
+})
+export class ShellComponent {
+  //Breakpoints.Handset if it matches typical mobile size
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe([Breakpoints.Handset])
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    )
+
+  constructor(private breakpointObserver: BreakpointObserver){
+
+  }
+}
