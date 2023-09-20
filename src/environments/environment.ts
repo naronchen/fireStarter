2bab4632b76d917a92b56e27349b546f33d59ebb
Naron <naron.chen@richmond.edu>
2023-09-19 20:16:55 -0400

    firebase setUp

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyAW-9q5kWFxiyc4hTR03Ua0BzPXGz_qPGs",
+        authDomain: "firestarter-c5c55.firebaseapp.com",
+        projectId: "firestarter-c5c55",
+        storageBucket: "firestarter-c5c55.appspot.com",
+        messagingSenderId: "1037973700833",
+        appId: "1:1037973700833:web:9ac04bc24d97f9aac577a0",
+        measurementId: "G-NXSY6KRZBS"
+    }
+};
